7c1f48f3bb80a95219bed641e9ee31c63fbbf208
mradulpaliwal <138373359+mradulpaliwal@users.noreply.github.com>
2024-08-20 23:50:45 +0530

    Time: 16 ms (67.30%), Space: 27 MB (44.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,25 +1,55 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       int n=nums.size();
+        int n=nums.size();
        
-        
-        for(int i=0;i<n;i++){
-            int count=1;
+       // brute force soln 
+        //for(int i=0;i<n;i++){
+            //int count=1;
             
-            for (int j=i+1;j<n;j++)
-                if (nums[i]==nums[j])
-                    count++;
+          //  for (int j=i+1;j<n;j++)
+               // if (nums[i]==nums[j])
+                    //count++;
                     
                 
         
                 
             
-        if(count>n/2)
-                return nums[i];}
+        //if(count>n/2)
+                //return nums[i];
+    //}
             
         
-        return -1;
+        //return -1;
+        // moore voting algorithm
+        // sbse phle dundenge majority konsa hai
+        int res=0;
+        int count=1;
         
+        for(int i=1;i<n;i++){{
+            
+            if(nums[i]==nums[res]){
+                count++;
+            }
+            else count--;}
+            if (count==0){
+                res=i;count=1;
+            }
+            
+        }
+        // check kro majority he bhi yha nahi
+                         
+       count=0;
+            
+
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]==nums[res]){
+                count++;
+            }
+        }
+        if(count<=n/2){
+        res=-1;}
+        return nums[res];
     }
 };
